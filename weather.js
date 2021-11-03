8db50a3756f1e649283c55ad34d93c3287978f5e
Amarjit Ghosh <amarjitghosh31@gmail.com>
2021-11-03 21:07:04 +0530

    new-api

---
+++ weather.js
@@ -0,0 +1,58 @@
+window.addEventListener('load',()=>{
+    var lat;
+    var long;
+    var Temperature=document.querySelector('.temp');
+    var Description=document.querySelector(".description");
+    var place=document.querySelector('.location');
+    var temsec=document.querySelector("span");
+
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            lat=position.coords.latitude;
+            long=position.coords.longitude;
+
+     const api=`https:\\api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=179ece0c68a14006518df0b24d5c7ce7`;
+    
+    fetch(api)
+        .then(response=>{
+            return response.json();
+        })
+        .then(data=>{
+            console.log(data);
+          var TemperatureValue=data['main']['temp'];
+          var WeatherVlaue=data['weather']['0']['description'];
+          var Namevalue=data['name'];
+          var icon=data['weather']['0']['desciption'];
+
+          var c=TemperatureValue-273.15;
+          Temperature.innerHTML=Math.floor(c);
+          Description.innerHTML=WeatherVlaue;
+          place.innerHTML=Namevalue;
+
+          var FH=((c*9)/5)+32;
+        
+        Temperature.addEventListener('click',()=>{
+
+       
+         if(temsec.innerText=="C")
+         {
+             temsec.innerText="F";
+             Temperature.innerHTML=Math.floor(FH);
+         }
+         else
+         {
+            temsec.innerText="C";
+            Temperature.innerHTML=Math.floor(c);
+         }
+        });
+          
+           
+        });
+
+        });
+     
+        
+    }
+  
+});
